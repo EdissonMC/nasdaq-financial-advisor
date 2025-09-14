9abde55bd3b96afe95f29ea2daf5c76968ab4f63
santiagochopra <santiagochopra@gmail.com>
2025-09-14 00:25:31 -0300

    Docker - minimal app -v.4. -blob polarities added.

--- sentiment_analysis/logging_service/app/analysis.py
+++ sentiment_analysis/logging_service/app/analysis.py
@@ -1,6 +1,8 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
+from textblob import TextBlob
+
 
 # Ensure the lexicon is available
 nltk.download("vader_lexicon")
@@ -15,7 +17,10 @@ _vectorizer = CountVectorizer(stop_words='english')
 def analyze_text(text: str):
     """
     Analyzes the input text for sentiment and Bag-of-Words features.
-    Returns a dictionary with sentiment scores and BoW vector.
+    Subjectivity: Measures the subjectivity of the text. A value close to 1 
+    means the text is highly subjective (opinion), while 0 means the text 
+    is more objective (factual).
+    Returns a dictionary with sentiment scores, BoW vector and Textblob polarities.
     """
     
     # Sentiment
@@ -28,4 +33,11 @@ def analyze_text(text: str):
 
     bow_dict = {word: int(count) for word, count in zip(bow_features, bow_counts)}
 
-    return {"sentiment": sentiment, "bow": bow_dict}
+    # Subjectivity
+    blob = TextBlob(text)
+    blob_sentiment = blob.sentiment
+    polarity = blob_sentiment.polarity
+    subjectivity = blob_sentiment.subjectivity
+
+
+    return {"sentiment": sentiment, "bow": bow_dict, "blob_polarity": {"polarity": polarity, "subjectivity": subjectivity}}
